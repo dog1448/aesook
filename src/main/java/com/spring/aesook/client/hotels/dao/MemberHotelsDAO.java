b333aba09caacc16a4295d03bc23773c5c5b3d64
yopyzzang <yopyzzang@gmail.com>
2020-03-12 20:54:14 +0900

    hotelMod

--- src/main/java/com/spring/aesook/client/hotels/dao/MemberHotelsDAO.java
+++ src/main/java/com/spring/aesook/client/hotels/dao/MemberHotelsDAO.java
@@ -3,10 +3,14 @@ package com.spring.aesook.client.hotels.dao;
 import java.util.List;
 
 import com.spring.aesook.client.hotels.vo.MemberHotelsVO;
+import com.spring.aesook.client.hotels.vo.MemberRoomVO;
 import com.spring.aesook.client.member.vo.MemberVO;
 
 public interface MemberHotelsDAO {
 	void insertHotels(MemberHotelsVO vo);
 	List<MemberHotelsVO> getMyHotels(MemberVO vo);
 	int getHotelsCode();
+	void modifyHotels(MemberHotelsVO memberHotelsVO);
+	void modifyRooms(MemberRoomVO memberRoomVO);
+
 }
--- src/main/java/com/spring/aesook/client/hotels/dao/MemberHotelsDAOImpl.java
+++ src/main/java/com/spring/aesook/client/hotels/dao/MemberHotelsDAOImpl.java
@@ -2,6 +2,7 @@ package com.spring.aesook.client.hotels.dao;
 
 import java.util.List;
 
+import com.spring.aesook.client.hotels.vo.MemberRoomVO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -30,6 +31,17 @@ public class MemberHotelsDAOImpl implements MemberHotelsDAO{
 	@Override
 	public int getHotelsCode() {		
 		return sqlSessionTemplate.selectOne(namespace + ".selectHotelsCode");
+	}
+
+	@Override
+	public void modifyHotels(MemberHotelsVO memberHotelsVO) {
+		sqlSessionTemplate.update(namespace+".modifyHotels",memberHotelsVO);
+	}
+
+	@Override
+	public void modifyRooms(MemberRoomVO memberRoomVO) {
+		sqlSessionTemplate.update(namespace+".modifyRoom",memberRoomVO);
+
 	}
 
 }
