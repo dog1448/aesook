fcaa2940705222fe9fb4ba86dce95e3e36fe4481
DJ Lee <dog1448@gmail.com>
2020-03-12 23:51:41 +0900

    addmore

--- src/main/java/com/spring/aesook/client/hotels/service/MemberInsertHotelsServiceImpl.java
+++ src/main/java/com/spring/aesook/client/hotels/service/MemberInsertHotelsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.spring.aesook.client.hotels.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.spring.aesook.client.hotels.dao.MemberHotelsDAO;
@@ -9,7 +10,8 @@ import com.spring.aesook.client.hotels.vo.MemberHotelsFacilityVO;
 import com.spring.aesook.client.hotels.vo.MemberHotelsVO;
 import com.spring.aesook.client.member.vo.MemberVO;
 
-public class MemberInseretHotelsServiceImpl implements MemberInsertHotelsService {
+@Service("memberInsertHotelsService")
+public class MemberInsertHotelsServiceImpl implements MemberInsertHotelsService {
 
 	@Autowired
 	private MemberHotelsFacilityDAO memberHotelsFacilityDAO;
